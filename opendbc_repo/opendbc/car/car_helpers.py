8e3bea0357f51c6a518a509af38f07e3c1b58381
SelfdriveVanagon <41362285+Lukilink@users.noreply.github.com>
2025-07-26 18:19:54 +0200

    Update car_helpers.py

--- opendbc_repo/opendbc/car/car_helpers.py
+++ opendbc_repo/opendbc/car/car_helpers.py
@@ -154,7 +154,7 @@ def get_car(can_recv: CanRecvCallable, can_send: CanSendCallable, set_obd_multip
 
   if candidate is None:
     carlog.error({"event": "car doesn't match any fingerprints", "fingerprints": repr(fingerprints)})
-    candidate = "CAR.TOYOTA_COROLLA"
+    candidate = "MOCK"
 
   CarInterface = interfaces[candidate]
   CP: CarParams = CarInterface.get_params(candidate, fingerprints, car_fw, alpha_long_allowed, is_release, docs=False)
